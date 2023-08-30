fa5c8dfda76537e24439d54a8b1e580ce6fce666
AstrlJelly <bdlawson115@gmail.com>
2023-08-30 18:57:13 -0400

    commands use classes + chains
    bot is now kil using $kill

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const { Client, Events, GatewayIntentBits, GuildChannel, Emoji } = require('discord.js');
 const { token } = require('./config.json');
 const { wordsToNumbers } = require('words-to-numbers');
-const { evaluate } = require('mathjs');
+const { evaluate, boolean } = require('mathjs');
 const { count } = require('console');
 
 // Create a new client instance
@@ -15,103 +15,123 @@ const client = new Client({
     ]
 });
 
-const people = [
-    { 
-        userId:"", 
-        strikes:0, 
-        desc:""
+class Person {
+    constructor(userId, strikes, desc) {
+        this.userId = userId;
+        this.strikes = strikes;
+        this.desc = desc;
     }
-];
+}
+
+class Command {
+    constructor(type, commandName, desc, func, params = [], limitedTo = []) {
+        this.type = type;
+        this.commandName = commandName;
+        this.desc = desc;
+        this.func = func;
+        this.params = params;
+        this.limitedTo = limitedTo;
+    }
+}
+
+class Param {
+    constructor(paramName, desc, type, preset = null) {
+        this.paramName = paramName;
+        this.desc = desc;
+        this.type = type;
+        this.preset = preset;
+    }
+}
 
 const commands = [
-    { 
-        commandName: "help",
-        desc: "lists all commands",
-        limitedTo: [],
-        params: [],
-        command: function(message)
-        {
-            let response = message.author.toString() + "\n";
-            for (let i = 0; i < commands.length; i++) {
-                let element = commands[i];
-                response += "$"+element.commandName + " : " + element.desc + "\n";
-            }
-            message.reply(response);
+    new Command("bot", "help", "lists all commands", function(message) {
+        let response = message.author.toString() + "\n";
+        if (this.params[0])
+        for (let i = 0; i < commands.length; i++) {
+            let element = commands[i];
+            response += `$${element.commandName} : ${element.desc} \n`;
         }
-    },
-    {
-        commandName: "eval",
-        desc: "does the math put in front of it",
-        limitedTo: [ 
-            "438296397452935169" 
-        ],
-        params: [],
-        command: function(message)
-        {
-            var eval = '';
-            try {
-                eval = evaluate(message.content.substring(6, message.content.length));
-            } catch (error) {
-                eval = error;
-            }
-            message.reply(String(eval));
-            return;
+        message.reply(response);
+    }, [new Param("whichCommand", "", typeof(String))], []),
+
+    new Command("fun", "eval", "does the math put in front of it", function(message) {
+        var eval = '';
+        try {
+            eval = evaluate(message.content.substring(6, message.content.length));
+        } catch (error) {
+            eval = error;
         }
-    },
-    {
-        commandName: "echo",
-        desc: "echoes whatever's in front of it",
-        limitedTo: [],
-        params: [],
-        command: function(message)
-        {
-            let reply = "something broke";
-            try {
-                reply = message.content.substring(this.commandName.length + 2);
-            } catch (error) {
-                reply = error;
-            } finally {
-                message.channel.send(reply);
-            }
-            
-            return;
+        message.reply(String(eval));
+    }, [], [ "438296397452935169" ]),
+    
+    new Command("fun", "echo", "echoes whatever's in front of it", function(message) {
+        let reply = "something broke";
+        try {
+            reply = message.content.split(this.commandName.length + 2);
+        } catch (error) {
+            reply = error;
+        } finally {
+            message.channel.send(reply);
         }
-    },
-    {
-        commandName: "countHere",
-        desc: "sets the current channel to be the channel used for counting",
-        limitedTo: [ 
-            "438296397452935169" 
-        ],
-        params: [],
-        command: function(message) 
-        {
+    }, [], [ "438296397452935169" ]),
+    
+    new Command("patterns/counting", "countHere", "sets the current channel to be the channel used for counting", function(message) {
+        if (countingChannel = message.channel.id) {
+            countingChannel = "";
+            message.reply('counting in this channel has ceased.');
+        } else {
             countingChannel = message.channel.id;
             message.reply('alright. start counting then.');
-            return;
         }
-    },
-    {
-        commandName: "chainHere",
-        desc: "sets the current channel to be the channel used for message chains",
-        limitedTo: [ 
-            "438296397452935169" 
-        ],
-        params: [],
-        command: function(message) 
-        {
+    }, [], [ "438296397452935169" ]),
+    
+    new Command("patterns/counting", "resetCount", "resets the current count", function(message) {
+        resetNumber(message, 'reset the count!', '✅');
+    }, [], [ "438296397452935169" ]),
+
+    new Command("patterns/chaining", "chainHere", "sets the current channel to be the channel used for message chains", function(message) {
+        if (chainChannel === message.channel.id) {
+            chainChannel = "";
+            message.reply('the chain in this channel has been eliminated.');
+        } else {
             chainChannel = message.channel.id;
             message.reply('alright. start a chain then.');
-            return;
         }
-    },
+        
+    }, [], [ "438296397452935169" ]),
+
+    new Command("patterns/chaining", "autoChain", "sets the current channel to be the channel used for message chains", function(message) {
+        if (chainChannel === message.channel.id) {
+            chainChannel = "";
+            message.reply('the chain in this channel has been eliminated.');
+        } else {
+            chainChannel = message.channel.id;
+            message.reply('alright. start a chain then.');
+        }
+    }, [ new Param("howMany", "how many messages in a row does it take for the chain to trigger?", typeof(Number), 4) ], [ "438296397452935169" ]),
+    
+    new Command("bot", "kill", "kills the bot", function(message) {
+        message.channel.send('bot is now dead 😢');
+        client.destroy();
+    }, [], 
+    [
+        "438296397452935169",
+        "705120334705197076",
+        "686222324860715014",
+    ]),
 ];
 
-
+// counting variables
 var currentNumber = 0; // the last number said that was correct
 var prevNumber = 0;    // used to reset back to the last number if i messed up my code
-var highestNumber = 0; // the highest number ever gotten to 
+var highestNumber = 0; // the highest number ever gotten to
 var lastCounter = "";  // used to check for duplicates
+
+// chain variables
+var currentChain = "";
+var chainAmount = 0;
+var prevChain = "";
+var lastChainer = "";
 
 // are set using commands
 var countingChannel = "";
@@ -120,21 +140,13 @@ var chainChannel = "";
 // blacklist list, the function to push to it is blacklist()
 const bl = [];
 
-function isMe(user) {
-    return user.id === '438296397452935169';
-}
-
-// function isBlacklisted(user) {
-//     return p1 * p2;
-// }
-
-async function resetNumber(message, reply = 'empty. astrl screwed up lol')
+async function resetNumber(message, reply = 'empty. astrl screwed up lol', react = '💀')
 {
     if (currentNumber > highestNumber) highestNumber = currentNumber;
     lastCounter = '';
     prevNumber = currentNumber;
     currentNumber = 0;
-    message.react('💀');
+    message.react(react);
     await message.reply(reply);
 }
 
@@ -154,8 +166,9 @@ client.on(Events.MessageCreate, async message => {
     for (let i = 0; i < commands.length; i++) {
         let com = commands[i];
         if (("$"+com.commandName) === message.content.substring(0, com.commandName.length + 1)) {
-            if (commands.limitedTo === undefined || com.limitedTo.includes(message.author.id)) {
-                com.command(message);
+            if (com.limitedTo.count === undefined || com.limitedTo.includes(message.author.id)) {
+                com.func(message);
+                return;
             } else {
                 await message.reply('hey, you can\'t use this command!');
             }
@@ -178,7 +191,7 @@ client.on(Events.MessageCreate, async message => {
                 try {
                     num = parseInt(content);
                 } catch (error) {
-                    message.reply('yeah that doesn\'t work. sorry \n');
+                    message.reply('yeah that doesn\'t work. sorry \n' + error);
                 }
             }
             return;
@@ -200,8 +213,26 @@ client.on(Events.MessageCreate, async message => {
             );
         }
     } else if (message.channel.id === chainChannel) {
-        message.channel.send('chains aren\'t set up right now, astrl will add them soon');
-        chainChannel = "";
+        if (!currentChain) {
+            currentChain = message.content.toLowerCase();
+            chainAmount = 1;
+            return;
+        }
+        if (message.content.toLowerCase() === currentChain && lastChainer !== message.author.id) {
+            chainAmount++;
+            if (chainAmount >= 3) message.react('⛓️');
+        } else {
+            if (chainAmount >= 3) message.react('💔');
+            currentChain = message.content.toLowerCase();
+            chainAmount = 1;
+        }
+        lastChainer = message.author.id;
+        // if (message.content.toLowerCase() === currentChain) {
+        //     chainAmount++;
+        //     if (chainAmount > 3) message.react('⛓️');
+        // } else if (chainAmount < 3) {
+        //     message.react('⛓️');
+        // }
     }
 });
 
